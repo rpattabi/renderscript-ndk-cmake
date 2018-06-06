bf2b61289df043de62e388ca99feec717bc90aac
rpattabi <ra@gu.in>
2018-06-06 19:12:50 +0530

    checking in final set of changes, anyways renderscript support mode didn't work.

--- app/libs/rsKernels_jni/jni/Android.mk
+++ app/libs/rsKernels_jni/jni/Android.mk
@@ -1,23 +1,16 @@
 LOCAL_PATH := $(call my-dir)
+INCLUDE_PATH := $(NDK_ROOT)/sources/cxx-stl/system/include
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := rsKernels_jni
 
 RS_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.rs)
-RS_SRC_FILES := $(RS_SRC_FILES:$(LOCAL_PATH)/%=%)
-
 CPP_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.cpp)
-CPP_SRC_FILES := $(CPP_SRC_FILES:$(LOCAL_PATH)/%=%)
-
-CPP_HDR_FILES := $(wildcard $(LOCAL_PATH)/*.h)
-CPP_HDR_FILES := $(CPP_HDR_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := $(CPP_SRC_FILES) $(RS_SRC_FILES)
-LOCAL_C_INCLUDES := $(CPP_HDR_FILES)
+LOCAL_C_INCLUDES := $(INCLUDE_PATH) $(LOCAL_PATH)
 
 LOCAL_CLANG := true
-
-LOCAL_CFLAGS := -std=c++11
 LOCAL_LDFLAGS := -Wl,-Bsymbolic
 
 LOCAL_LDLIBS := \
@@ -25,10 +18,11 @@ LOCAL_LDLIBS := \
 	-llog \
 
 LOCAL_RENDERSCRIPT_COMPATIBILITY := true
-
+LOCAL_RENDERSCRIPT_FLAGS := -rs-package-name=android.support.v8.renderscript
 LOCAL_SHARED_LIBRARIES := RSSupport blasV8 RSSupportIO
 LOCAL_STATIC_LIBRARIES := RScpp_static
 
+LOCAL_SDK_VERSION := current
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/renderscript)
--- app/libs/rsKernels_jni/jni/Application.mk
+++ app/libs/rsKernels_jni/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_ABI := armeabi-v7a x86
-APP_PLATFORM := android-27
-APP_STL := c++_static
 NDK_TOOLCHAIN_VERSION := clang
+APP_ABI := armeabi-v7a x86
+APP_CPPFLAGS += -std=c++11 -v
+APP_STL := c++_shared
+APP_OPTIM := release
